475cf5de1decdacb680c4da722f8e6425fa1efc1
Herman Hermitage <hermanhermitage@hotmail.com>
2012-07-14 23:04:20 +1000

    Fixed tabs

--- blinker01/blinker01.s
+++ blinker01/blinker01.s
@@ -3,45 +3,45 @@
 // See: https://github.com/dwelch67/raspberrypi/tree/master/blinker01)
 //-------------------------------------------------------------------------
 /*
-				extern void PUT32 ( unsigned int, unsigned int );
-				extern unsigned int GET32 ( unsigned int );
-				extern void dummy ( unsigned int );
+        extern void PUT32 ( unsigned int, unsigned int );
+        extern unsigned int GET32 ( unsigned int );
+        extern void dummy ( unsigned int );
 
-				#define GPFSEL1 0x20200004
-				#define GPSET0  0x2020001C
-				#define GPCLR0  0x20200028
+        #define GPFSEL1 0x20200004
+        #define GPSET0  0x2020001C
+        #define GPCLR0  0x20200028
 
-				int notmain ( void )
-				{
-						unsigned int ra;
+        int notmain ( void )
+        {
+            unsigned int ra;
 
-						ra=GET32(GPFSEL1);
-						ra&=~(7<<18);
-						ra|=1<<18;
-						PUT32(GPFSEL1,ra);
+            ra=GET32(GPFSEL1);
+            ra&=~(7<<18);
+            ra|=1<<18;
+            PUT32(GPFSEL1,ra);
 
-						while(1)
-						{
-								PUT32(GPSET0,1<<16);
-								for(ra=0;ra<0x100000;ra++) dummy(ra);
-								PUT32(GPCLR0,1<<16);
-								for(ra=0;ra<0x100000;ra++) dummy(ra);
-						}
-						return(0);
-				}
+            while(1)
+            {
+                PUT32(GPSET0,1<<16);
+                for(ra=0;ra<0x100000;ra++) dummy(ra);
+                PUT32(GPCLR0,1<<16);
+                for(ra=0;ra<0x100000;ra++) dummy(ra);
+            }
+            return(0);
+        }
 */
 
   START("blinker01.bin");
 
-	equ(GPFSEL1, 0x7e200004);
-	equ(GPSET0, 0x7e20001c);
-	equ(GPCLR0, 0x7e200028);
+  equ(GPFSEL1, 0x7e200004);
+  equ(GPSET0, 0x7e20001c);
+  equ(GPCLR0, 0x7e200028);
 
-	declare(loop);
-	declare(delayloop1);
-	declare(delayloop2);
+  declare(loop);
+  declare(delayloop1);
+  declare(delayloop2);
 
-	fillb(0x200, 0);
+  fillb(0x200, 0);
 
   movi(r1, GPFSEL1);
   ld(r0, r1);
@@ -59,7 +59,7 @@ label(loop);
   movi(r0, 0);
 label(delayloop1);
   addi(r0, 1);
-	cmpi(r0, 0x100000);
+  cmpi(r0, 0x100000);
   bne(delayloop1);
  
   st(r3, r2);
